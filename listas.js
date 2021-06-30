1e150cfc3dff4cebe1f6fde828ee64137a0940fc
Augusto <lagrange.tesla@gmail.com>
2021-06-30 15:17:33 -0500

    Searching through a list

--- listas.js
+++ listas.js
@@ -24,3 +24,21 @@ db.users.updateOne(
 db.users.findOne({
   courses: ['Python', 'MongoDB', 'SQL', 'Java'],
 })
+
+// $all -> nos permite hacer búsquedas dentro de listas
+// $all funciona como el operador and, las condiciones que pongamos deben ser verdaderas
+// Ejercicio: obtener todos los usarios que posean por curso MongoDB
+db.users.find({courses: { $all: ['MongoDB'] }}).pretty()
+
+// Ejercicio: obtener todos los usarios que posean por curso MongoDB o SQL
+db.users.find({courses: { $all: ['MongoDB','SQL'] }}).pretty()
+
+// Multiples condiciones
+db.users.find({courses: 'SQL'}).pretty()
+
+// operador $and
+db.users.find({$and: [{courses: 'SQL'},{courses:'MongoDB'}]}).pretty()
+
+// Operador $or
+db.users.find({$or: [{courses: 'SQL'},{courses:'Git'}]}).pretty()
+
